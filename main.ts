639c11969dc3a034e9c6e03e04b714098e4ed44b
zptilden325 <71046087+zptilden325@users.noreply.github.com>
2022-04-01 12:25:04 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+let increase = 7
+let mySprite = sprites.create(img`
+    . . . . . . b b b b . . . . . . 
+    . . . . . . b 4 4 4 b . . . . . 
+    . . . . . . b b 4 4 4 b . . . . 
+    . . . . . b 4 b b b 4 4 b . . . 
+    . . . . b d 5 5 5 4 b 4 4 b . . 
+    . . . . b 3 2 3 5 5 4 e 4 4 b . 
+    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+    . . . b 5 3 2 3 5 5 5 5 e e e e 
+    . . b d 7 5 5 5 3 2 3 5 5 e e e 
+    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+    b d 3 2 d 5 5 5 d d d 4 4 . . . 
+    b 5 5 5 5 d d 4 4 4 4 . . . . . 
+    4 d d d 4 4 4 . . . . . . . . . 
+    4 4 4 4 . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.y += -7
+for (let index = 0; index < 10; index++) {
+    pause(200)
+    mySprite.x += increase
+    pause(200)
+    increase += 5
+    mySprite.y += increase
+    pause(200)
+    increase = -1 * increase
+    mySprite.x += increase
+    pause(200)
+    increase += -5
+    mySprite.y += increase
+    increase = -1 * increase
+}
